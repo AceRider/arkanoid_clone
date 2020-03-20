dacb82a6d211dbb31123b57962c7ac6b72533d66
aecio <aecio.jorge@gmail.com>
2020-03-19 21:33:27 -0400

    add ball e physics

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+   
+    public float speed = 100.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+}
