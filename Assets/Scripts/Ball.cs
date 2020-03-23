9eb67b4ebe1541c352256a3574636a31c1920e9d
aecio <aecio.jorge@gmail.com>
2020-03-22 23:55:02 -0400

    add scripts to player

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -12,4 +12,30 @@ public class Ball : MonoBehaviour
     {
         GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
     }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+       
+        if (col.gameObject.name == "racket")
+        {
+            float x = hitFactor(transform.position,
+                              col.transform.position,
+                              col.collider.bounds.size.x);
+
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketWidth)
+    {
+        // ascii art:
+        //
+        // 1  -0.5  0  0.5   1  <- x value
+        // ===================  <- racket
+        //
+        return (ballPos.x - racketPos.x) / racketWidth;
+    }
 }
